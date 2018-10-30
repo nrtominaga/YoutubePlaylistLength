ec3bb0f823423ed04c0c8ab39ddd9630480a5e95
Naoki Tominaga <u0876779@utah.edu>
2018-10-30 16:17:24 -0600

    home stretch

--- PlaylistLength.py
+++ PlaylistLength.py
@@ -1,4 +1,5 @@
 import argparse
+import isodate
 
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
@@ -7,23 +8,36 @@ from googleapiclient.errors import HttpError
 API_SERVICE_NAME = 'youtube'
 API_VERSION = 'v3'
 API_KEY = "AIzaSyCglRrhkgaiC6b3Bc6oW9Zb9XEVMbR2Yro"
+youtube = build(API_SERVICE_NAME, API_VERSION, developerKey=API_KEY)
 
 
 def get_playlist_info(playlist_id):
     # playlist info
-    youtube = build(API_SERVICE_NAME, API_VERSION, developerKey=API_KEY)
     part = "contentDetails"
     max_results = 50
     playlist_response = youtube.playlistItems().list(part=part, maxResults=max_results, playlistId=playlist_id).execute()
     return playlist_response
 
 
-def get_video_info():
-    print("hi")
+def get_video_info(ids):
+    vid_input = ','.join(ids)
+    return youtube.videos().list(id=vid_input, part='contentDetails').execute()
+
+def calculate_playlist_length(ids):
+    video_response = get_video_info(ids)
+    length = 0
+    for video in video_response['items']:
+        time = isodate.parse_duration(video['contentDetails']['duration'])
+        print(video['contentDetails']['duration'])
+        length += time.total_seconds()
+    return length
 
 
-def get_playlist_length(playlistInfo, client):
-    print("what")
+def get_video_ids(playlist_info):
+    vids = []
+    for video in playlist_info['items']:
+        vids.append(video['contentDetails']['videoId'])
+    return vids
 
 
 if __name__ == "__main__":
@@ -31,4 +45,5 @@ if __name__ == "__main__":
     print(playlistId)
 
     playlist = get_playlist_info(playlistId)
-    print(playlist)
+    vid_ids = get_video_ids(playlist)
+    length = calculate_playlist_length(vid_ids)
